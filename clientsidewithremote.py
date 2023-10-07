7e4e6b27fbc187f8ce3b45d5655f4ecf97619415
tripathikartik20 <66067513+tripathikartik20@users.noreply.github.com>
2023-10-07 22:39:01 +0530

    Add files via upload

--- clientsidewithremote.py
+++ clientsidewithremote.py
@@ -7,27 +7,21 @@ import tkinter as tk
 from tkinter import Label, Button, Canvas
 from PIL import Image, ImageTk
 
-# Constants
-SERVER_IP = "127.0.0.1"  # Replace with the IP address of the server machine
+SERVER_IP = "127.0.0.1"
 SERVER_PORT = 12345
 
-# Socket setup for receiving video frames and telemetry data
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.bind(("127.0.0.1", 65001))  # Bind to any available port
+client_socket.bind(("127.0.0.1", 65001))#use any port but try for same post as serverside
 
-# GUI
 root = tk.Tk()
 root.title("Remote Control GUI")
 
-# Create a canvas for video display
 canvas = Canvas(root, width=660, height=260)
 canvas.pack()
 
-# Create labels for telemetry data
 telemetry_label = Label(root, text="Telemetry Data: Speed: 0 Battery: 100")
 telemetry_label.pack()
 
-# Exit button
 def exit_program():
     client_socket.close()
     root.destroy()
@@ -35,12 +29,10 @@ def exit_program():
 exit_button = Button(root, text="Exit", command=exit_program)
 exit_button.pack()
 
-# Function to update video feed in the GUI
 def update_video():
     while True:
         data, server_address = client_socket.recvfrom(65535)
         if data:
-            # Extract video frames from received data
             frame_size = struct.unpack("<L", data[:4])[0]
             frame_data = data[4:4 + frame_size]
             frame = pickle.loads(frame_data)
@@ -55,7 +47,6 @@ def update_video():
 video_thread = threading.Thread(target=update_video)
 video_thread.start()
 
-# Function to receive and display telemetry data
 def receive_telemetry_data():
     while True:
         data, server_address = client_socket.recvfrom(1024)
--- serversidewithcam.py
+++ serversidewithcam.py
@@ -4,28 +4,24 @@ import struct
 import pickle
 import threading
 
-# Constants
-SERVER_IP = "127.0.0.1"  # Listen on all available network interfaces
-SERVER_PORT = 12345
+from clientsidewithremote import SERVER_PORT
+
+SERVER_IP = "127.0.0.1"
 MAX_PACKET_SIZE = 65507
 
-# Video streaming setup for two cameras
-cap1 = cv2.VideoCapture(0)  # Camera 1 (adjust index as needed)
-cap1.set(3, 320)  # Set the width of the video frame
-cap1.set(4, 240)  # Set the height of the video frame
+cap1 = cv2.VideoCapture(0)
+cap1.set(3, 320)
+cap1.set(4, 240)
 
-cap2 = cv2.VideoCapture(1)  # Camera 2 (adjust index as needed)
-cap2.set(3, 320)  # Set the width of the video frame
-cap2.set(4, 240)  # Set the height of the video frame
+cap2 = cv2.VideoCapture(1) 
+cap2.set(3, 320)
+cap2.set(4, 240)
 
-# Socket setup
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind((SERVER_IP, SERVER_PORT))
 
-# Telemetry data (simulated)
 telemetry_data = {"Speed": 0, "Battery": 100}
 
-# Functions to send video frames and telemetry data to the client
 def send_video(client_address, cam):
     while True:
         ret, frame = cam.read()
@@ -45,7 +41,6 @@ def send_telemetry_data(client_address):
         telemetry_msg = pickle.dumps(telemetry_data)
         server_socket.sendto(telemetry_msg, client_address)
 
-# Start video and telemetry threads for both cameras
 video_thread1 = threading.Thread(target=send_video, args=(("client_address", 12345), cap1))
 video_thread2 = threading.Thread(target=send_video, args=(("client_address", 12345), cap2))
 telemetry_thread = threading.Thread(target=send_telemetry_data, args=(("client_address", 12345),))
@@ -55,4 +50,4 @@ video_thread2.start()
 telemetry_thread.start()
 
 while True:
-    pass  # Keep the server running indefinitely
+    pass  
